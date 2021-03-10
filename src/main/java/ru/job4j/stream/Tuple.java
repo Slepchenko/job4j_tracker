d0f8e999c12aab06f46d116f8b603b432b2111c1
Satrim <59724286+Satrim@users.noreply.github.com>
2021-03-10 22:35:59 +0300

    2. Аттестация [#408988]

--- src/main/java/ru/job4j/stream/Tuple.java
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -14,6 +14,10 @@ public class Tuple {
         this.score = score;
     }
 
+    public double getScore() {
+        return score;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
