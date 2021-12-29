2fb24913724ac0e631870f8fd8bfd0156dd3d595
Satrim <59724286+Satrim@users.noreply.github.com>
2021-12-30 05:37:04 +1200

    for checkStyle

--- src/main/java/ru/job4j/stream/Tuple.java
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -2,9 +2,6 @@ package ru.job4j.stream;
 
 import java.util.Objects;
 
-
-//Класс Tuple содержит результаты: имя и баллы.
-// Этот класс используется как для учеников, так и для предметов.
 public class Tuple {
     private String name;
     private double score;
@@ -20,11 +17,15 @@ public class Tuple {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Tuple tuple = (Tuple) o;
-        return Double.compare(tuple.score, score) == 0 &&
-                Objects.equals(name, tuple.name);
+        return Double.compare(tuple.score, score) == 0
+               && Objects.equals(name, tuple.name);
     }
 
     @Override
