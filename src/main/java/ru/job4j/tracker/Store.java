e4cc4c369535af7b6cac4ed6d5b049ac3a23de76
Satrim <59724286+Satrim@users.noreply.github.com>
2022-01-06 20:15:31 +1200

    2. Трекер SQL [#1734 #245542]

--- src/main/java/ru/job4j/tracker/Store.java
+++ src/main/java/ru/job4j/tracker/Store.java
@@ -5,9 +5,14 @@ import java.util.List;
 
 public interface Store {
     Item add(Item item) throws Exception;
+
     boolean replace(int id, Item item) throws Exception;
+
     boolean delete(int id) throws Exception;
+
     List<Item> findAll() throws Exception;
+
     List<Item> findByName(String key) throws SQLException;
+
     Item findById(int id) throws SQLException;
 }
