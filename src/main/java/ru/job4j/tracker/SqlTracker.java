a7df71d4897b095b05cd4aeafed9ac3acd650e0d
Satrim <59724286+Satrim@users.noreply.github.com>
2022-01-08 19:34:54 +1200

    2. Трекер SQL [#1734 #245542]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -7,11 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-public class SqlTracker<E> implements Store, AutoCloseable {
+public class SqlTracker implements Store, AutoCloseable {
 
     private Connection cn;
-
-    private PreparedStatement preparedStatement;
 
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader()
@@ -37,112 +35,105 @@ public class SqlTracker<E> implements Store, AutoCloseable {
     }
 
     @Override
-    public Item add(Item item) throws Exception {
-        Timestamp timestamp = Timestamp.valueOf(item.getCreated());
-        connection(String.format("insert into items(name, created) values('%s', '%s');",
-                item.getName(),
-                timestamp));
+    public Item add(Item item) {
+        try (PreparedStatement statement =
+                     cn.prepareStatement("insert into items(name, created) values(?, ?);",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, item.getName());
+            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return item;
     }
 
     @Override
-    public boolean replace(int id, Item item) throws Exception {
-        Timestamp timestamp = Timestamp.valueOf(item.getCreated());
-        if (isExistIndex(id)) {
-            connection(String.format("update items set name = '%s', created = '%s' where id = '%s'",
-                    item.getName(),
-                    timestamp,
-                    id));
-            return true;
+    public boolean replace(int id, Item item) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     cn.prepareStatement("update items set name = ?, created = ? where id = ?",
+                             Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, item.getName());
+            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
+            statement.setInt(3, id);
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return false;
+        return result;
     }
 
     @Override
-    public boolean delete(int id) throws Exception {
-        if (isExistIndex(id)) {
-            connection(String.format("delete from items where id = %s",
-                    id));
-            return true;
+    public boolean delete(int id) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     cn.prepareStatement("delete from items where id = ?")) {
+            statement.setInt(1, id);
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return false;
+        return result;
     }
 
     @Override
-    public List<Item> findAll() throws Exception {
-        preparedStatement = cn.prepareStatement("select id, name, created from items");
-        return foundItems();
+    public List<Item> findAll() {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement("select * from items")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Timestamp itemCreated = resultSet.getTimestamp("created");
+                    LocalDateTime createdLocalDateTime = itemCreated.toLocalDateTime();
+                    Item item = new Item(resultSet.getString("name"));
+                    item.setId(resultSet.getInt("id"));
+                    item.setCreated(createdLocalDateTime);
+                    items.add(item);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
     }
 
     @Override
-    public List<Item> findByName(String key) throws SQLException {
-        preparedStatement = cn.prepareStatement(String.format(
-                "select id, name, created from items where name = '%s'",
-                key));
-        return foundItems();
+    public List<Item> findByName(String key) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement(String.format(
+                "select * from items where name = '%s'", key))) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Timestamp itemCreated = resultSet.getTimestamp("created");
+                    LocalDateTime createdLocalDateTime = itemCreated.toLocalDateTime();
+                    Item item = new Item(resultSet.getString("name"));
+                    item.setId(resultSet.getInt("id"));
+                    item.setCreated(createdLocalDateTime);
+                    items.add(item);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
     }
 
     @Override
     public Item findById(int id) throws SQLException {
-        preparedStatement = cn.prepareStatement(String.format(
-                "select name, created from items where id = '%s'",
-                id));
-        ResultSet resultSet = preparedStatement.executeQuery();
-        Item item = null;
-        if (isExistIndex(id)) {
-            while (resultSet.next()) {
-                String itemName = resultSet.getString("name");
+        Item item;
+        try (PreparedStatement statement = cn.prepareStatement(String.format(
+                "select * from items where id = '%s'",
+                id))) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                item = new Item(resultSet.getString("name"));
                 Timestamp itemCreated = resultSet.getTimestamp("created");
                 LocalDateTime createdLocalDateTime = itemCreated.toLocalDateTime();
-                item = new Item(itemName);
                 item.setId(id);
                 item.setCreated(createdLocalDateTime);
             }
         }
         return item;
-    }
-
-    private void connection(String sql) throws Exception {
-        try (Statement statement = cn.createStatement()) {
-            statement.execute(sql);
-        }
-    }
-
-    private boolean isExistIndex(int id) throws SQLException {
-        /**
-         Уважаемые менторы! Пользуясь случаем, хотел вас спросить, если вы это читаете,
-         почему такой вариант не работает?:
-
-         preparedStatement = cn.prepareStatement(String.format(
-         "select case when id = %s then 1 else 0 end as result from items where id = %s",
-         id, id));
-         */
-
-        preparedStatement = cn.prepareStatement(
-                "select 1 as result from items where id =" + id
-        );
-        ResultSet resultSet = preparedStatement.executeQuery();
-        int result = 0;
-
-        while (resultSet.next()) {
-            result = resultSet.getInt("result");
-        }
-        return result == 1;
-    }
-
-    private List<Item> foundItems() throws SQLException {
-        List<Item> items = new ArrayList<>();
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            int itemID = resultSet.getInt("id");
-            String itemName = resultSet.getString("name");
-            Timestamp itemCreated = resultSet.getTimestamp("created");
-            LocalDateTime createdLocalDateTime = itemCreated.toLocalDateTime();
-            Item item = new Item(itemName);
-            item.setId(itemID);
-            item.setCreated(createdLocalDateTime);
-            items.add(item);
-        }
-        return items;
     }
 }
